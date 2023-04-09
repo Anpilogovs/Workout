63ccb793e5398b66111a1c31478eaaa5b7be80c2
AnpilogovSergei <anpilogove.s@gmail.com>
2023-04-09 16:41:39 +0300

    #20_API change to display the weather depending on the day

--- Workout/Workout/Network/NetworkRequest.swift
+++ Workout/Workout/Network/NetworkRequest.swift
@@ -7,10 +7,12 @@ class NetworkRequest {
     func requestData(completion: @escaping (Result<Data, Error>) -> Void) {
         
         let key = "3bef07ccea8211abf63ae8583581acdd"
-        let latitude = 48.3794
-        let longitude = 31.1656
+        let latitude =  50.4501
+        let longitude = 30.5234
+        let date = DateComponents(calendar: .current, year: 2023, month: 4, day: 9).date!
+        let timestamp = Int(date.timeIntervalSince1970)
 
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(key)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&dt=\(timestamp)&appid=\(key)"
 
         guard let url = URL(string: urlString) else { return }
         
